2621f7cb2b4cdb8da9b819da4949fc0419d76bbf
Abubakar-telecard <abubakar_baig@telecard.com.pk>
2025-07-03 13:03:49 +0500

    Reusable Components

--- 2-create-react-comp/src/App.jsx
+++ 2-create-react-comp/src/App.jsx
@@ -1,10 +1,14 @@
 import Hello from "./Hello";
 import KgButton from "./KgButton";
+import Random from "./Random";
 
 function App() {
     return <div>
         <h1>This is the react course</h1>
         <Hello />
+        <Random />
+        <Random />
+        <Random />
         <KgButton></KgButton>
     </div>
 }
