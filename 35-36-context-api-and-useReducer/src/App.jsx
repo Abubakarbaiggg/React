8c33cd1a8b8f706a35e5146ba87b076a9dacd555
Abubakar-telecard <abubakar_baig@telecard.com.pk>
2025-07-08 16:11:48 +0500

    7-Social-media

--- 35-36-context-api-and-useReducer/src/App.jsx
+++ 35-36-context-api-and-useReducer/src/App.jsx
@@ -2,39 +2,19 @@ import AddTodo from "./components/AddTodo";
 import AppName from "./components/AppName";
 import "./App.css";
 import TodoItems from "./components/TodoItems";
-import { useState } from "react";
 import WelcomeMessage from "./components/WelcomeMessage";
-import { TodoItemsContext } from "./store/todo-items-store";
+import TodoItemsContextProvider from "./store/todo-items-store";
 
 function App() {
-  const [todoItems, setTodoItems] = useState([]);
-  const addNewItem = (itemName, itemDueDate) => {
-    const newTodoItems = [
-      ...todoItems,
-      { name: itemName, dueDate: itemDueDate },
-    ];
-    setTodoItems(newTodoItems);
-  };
-
-  const deleteItem = (todoItemName) => {
-    const newTodoItems = todoItems.filter((item) => item.name !== todoItemName);
-    setTodoItems(newTodoItems);
-  };
-  return (
-    <TodoItemsContext.Provider
-      value={{
-        todoItems,
-        addNewItem,
-        deleteItem,
-      }}
-    >
-      <center className="todo-container">
-        <AppName />
-        <AddTodo />
-        <WelcomeMessage />
-        <TodoItems />
-      </center>
-    </TodoItemsContext.Provider>
-  );
+    return (
+        <TodoItemsContextProvider>
+            <center className="todo-container">
+                <AppName />
+                <AddTodo />
+                <WelcomeMessage />
+                <TodoItems />
+            </center>
+        </TodoItemsContextProvider>
+    );
 }
 export default App;
