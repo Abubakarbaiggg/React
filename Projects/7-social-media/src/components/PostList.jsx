3a2e17643e4ff5666edec02e576b46fb68573770
Abubakar baig <Abubakar192005@gmail.com>
2025-07-09 00:41:24 +0500

    Comtinue Social media list completed and post create started.

--- Projects/7-social-media/src/components/PostList.jsx
+++ Projects/7-social-media/src/components/PostList.jsx
@@ -1,10 +1,16 @@
+import { useContext } from "react";
 import Post from "./Post";
+import { PostList as PostListData } from "../store/Post-list-store";
 
 const PostList = () => {
-    return <>
-        <Post />
-        <Post />
-        <Post />
-    </>
-}
+  const { postList } = useContext(PostListData);
+  return (
+    <div className="container mt-5">
+      {postList.map((post) => (
+        <Post key={post.id} post={post} />
+      ))}
+    </div>
+  );
+};
+
 export default PostList;
---
+++ Projects/7-social-media/src/store/Post-list-store.jsx
@@ -0,0 +1,67 @@
+import { useReducer, createContext } from "react";
+
+export const PostList = createContext({
+  postList: [],
+  addPost: () => {},
+  deletePost: () => {},
+});
+
+const postListReducer = (currPostList, action) => {
+  let newPostList = currPostList;
+  if (action.type === "DELETE_POST") {
+    newPostList = currPostList.filter(post=>post.id !== action.payload.postId)
+  }
+  return newPostList;
+};
+
+const PostListProvider = ({ children }) => {
+  const [postList, dispatchPostList] = useReducer(
+    postListReducer,
+    DEFAULT_POST_LIST
+  );
+
+  const addPost = () => {};
+
+  const deletePost = (postId) => {
+    console.log(`Deleted post called for : ${postId}`);
+    dispatchPostList({
+      type: "DELETE_POST",
+      payload: {
+        postId,
+      },
+    });
+  };
+
+  return (
+    <PostList.Provider
+      value={{
+        postList,
+        addPost,
+        deletePost,
+      }}
+    >
+      {children}
+    </PostList.Provider>
+  );
+};
+
+const DEFAULT_POST_LIST = [
+  {
+    id: "1",
+    title: "Going To Karachi",
+    body: "Hi Friends, I am going to Karachi for my vacations. Hope to enjoy a lot. Please out",
+    reactions: 2,
+    userId: "user-9",
+    tags: ["vacation", "karachi", "Enjoying"],
+  },
+  {
+    id: "2",
+    title: "Pass ho bhai",
+    body: "4 saal ki masti k baad bhi ho gaye hain pass. Hard to believe",
+    reactions: 15,
+    userId: "user-12",
+    tags: ["Graduating", "Unbelievable", "Enjoying"],
+  },
+];
+
+export default PostListProvider;
