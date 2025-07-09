edc308cdc3a2c61c6838cce9a1ad47825191538f
Abubakar baig <Abubakar192005@gmail.com>
2025-07-10 00:53:42 +0500

    Handling Loading State.

--- 37-advance-react/src/components/PostList.jsx
+++ 37-advance-react/src/components/PostList.jsx
@@ -1,31 +1,27 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect } from "react";
 import Post from "./Post";
 import { PostList as PostListData } from "../store/Post-list-store";
 import WelcomeMessage from "./Welcome";
 
 const PostList = () => {
-    const { postList, addInitialPosts } = useContext(PostListData);
-    const [dataFetched, setDataFetched] = useState(false)
+  const { postList, addInitialPosts } = useContext(PostListData);
 
-    if (!dataFetched) {
-        fetch('https://dummyjson.com/posts')
-            .then(res => res.json())
-            .then(data => {
-                addInitialPosts(data.posts);
-            });
-            setDataFetched(true)
-        }
+  useEffect(() => {
+    fetch("https://dummyjson.com/posts")
+      .then((res) => res.json())
+      .then((data) => {
+        addInitialPosts(data.posts);
+      });
+  },[]);
 
-    return (
-        <div className="container mt-5">
-            {postList.length === 0 && (
-                <WelcomeMessage />
-            )}
-            {postList.map((post) => (
-                <Post key={post.id} post={post} />
-            ))}
-        </div>
-    );
+  return (
+    <div className="container mt-5">
+      {postList.length === 0 && <WelcomeMessage />}
+      {postList.map((post) => (
+        <Post key={post.id} post={post} />
+      ))}
+    </div>
+  );
 };
 
 export default PostList;
