bb792904255021b32eefa12428e828c586796d5b
Abubakar baig <Abubakar192005@gmail.com>
2025-07-10 01:11:41 +0500

    The useEffect Hook Cleanup

--- 37-advance-react/src/components/PostList.jsx
+++ 37-advance-react/src/components/PostList.jsx
@@ -1,23 +1,30 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import Post from "./Post";
 import { PostList as PostListData } from "../store/Post-list-store";
 import WelcomeMessage from "./Welcome";
+import LoadingSpinner from "./LoadingSpinner";
 
 const PostList = () => {
   const { postList, addInitialPosts } = useContext(PostListData);
-
+  const [fetching, setFetching] = useState(false);
   useEffect(() => {
+    setFetching(true);
+    console.log("fetch started");
     fetch("https://dummyjson.com/posts")
       .then((res) => res.json())
       .then((data) => {
         addInitialPosts(data.posts);
+        setFetching(false);
+        console.log("fetch return");
       });
-  },[]);
+    console.log("fetch ended");
+  }, []);
 
   return (
     <div className="container mt-5">
-      {postList.length === 0 && <WelcomeMessage />}
-      {postList.map((post) => (
+      {fetching && <LoadingSpinner />}
+      {!fetching && postList.length === 0 && <WelcomeMessage />}
+      {!fetching && postList.map((post) => (
         <Post key={post.id} post={post} />
       ))}
     </div>
