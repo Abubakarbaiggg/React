991007eeb8b8287acb04fec098d3c26e7009089f
Abubakar-telecard <abubakar_baig@telecard.com.pk>
2025-07-10 12:04:35 +0500

    The useCallback Hook

--- Projects/8-pakistan-clock-version-two/src/components/CurrentTime.jsx
+++ Projects/8-pakistan-clock-version-two/src/components/CurrentTime.jsx
@@ -1,5 +1,19 @@
+import { useEffect, useState } from "react";
+
 let  CurrentTime = () => {
-    let time = new Date();
+
+    const [time,setTime] =  useState(new Date());
+    useEffect(()=>{
+        console.log("Interval has been setup");
+        const interval = setInterval(() => {
+            setTime(new Date());
+        }, 1000);
+        return () => {
+            clearInterval(interval);
+            console.log("cancelled the interval");
+        }
+    },[]);
+
     return <p className="lead">This is the current time :  {time.toLocaleDateString()} - { time.toLocaleTimeString()} </p>
 }
 export default CurrentTime;
